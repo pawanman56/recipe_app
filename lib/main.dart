4b2e9e69bab16abb5a89eb786928e78ff8ac3161
pawanman56 <pawanman56@gmail.com>
2019-01-11 17:18:59 +0545

    Helper methods initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:recipe_app/app.dart';
 
-void main() => runApp(
-  new RecipeApp(),
-);
+import 'package:recipe_app/app.dart';
+import 'package:recipe_app/state_widget.dart';
+
+void main() => runApp(new StateWidget(
+  child: new RecipeApp(),
+));
